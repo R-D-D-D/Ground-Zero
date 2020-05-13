62a914a364fe53c7a3b41ac2adbdbbe758758876
R-D-D-D <45935526+R-D-D-D@users.noreply.github.com>
2020-05-13 19:16:55 +0800

    remove build files

---
+++ waveform_processing/WaveformGenerator/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WaveformGenerator {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.openBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(13, 12);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(75, 21);
+            this.openBtn.TabIndex = 0;
+            this.openBtn.Text = "Open";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Enabled = false;
+            this.cancelBtn.Location = new System.Drawing.Point(94, 12);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 21);
+            this.cancelBtn.TabIndex = 1;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(608, 190);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 241);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.openBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ waveform_processing/WaveformGenerator/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Un4seen.Bass;
+
+namespace WaveformGenerator {
+    public partial class Form1 : Form {
+        private WaveformGenerator wg;
+
+        public Form1() {
+            InitializeComponent();
+
+            Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+        }
+
+        private async void openBtn_Click(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                openBtn.Enabled = false;
+                cancelBtn.Enabled = true;
+
+                wg = new WaveformGenerator(ofd.FileName);
+
+                // Change settings.
+                wg.Direction = WaveformGenerator.WaveformDirection.LeftToRight;
+                wg.Orientation = WaveformGenerator.WaveformSideOrientation.LeftSideOnTopOrLeft;
+                wg.Detail = 1.5f;
+                wg.LeftSideBrush = new SolidBrush(Color.Orange);
+                wg.RightSideBrush = new SolidBrush(Color.Gray);
+
+                wg.ProgressChanged += wg_ProgressChanged;
+                wg.Completed += wg_Completed;
+                wg.CreateStream();
+                 
+                await wg.DetectWaveformLevelsAsync();
+
+                // Add code to execute after completion. (Alternatively, add it in the wg_Completed method)
+                // ...
+            }
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e) {
+            wg.CancelDetection();
+        }
+
+        private void wg_Completed(object sender, EventArgs e) {
+            Console.WriteLine("Completed");
+
+            // Add code to execute after completion. (Alternatively, add it after "await wg.DetectWaveformLevelsAsync();")
+            // ...
+
+            wg.CloseStream();
+            openBtn.Enabled = true;
+            cancelBtn.Enabled = false;
+
+            ReloadWaveform();
+
+            //pictureBox1.Image.Save("Waveform.jpg");
+        }
+
+        private void wg_ProgressChanged(object sender, WaveformGenerator.ProgressChangedEventArgs e) {
+            ReloadWaveform();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e) {
+            if (pictureBox1.Image != null)
+                ReloadWaveform();
+        }
+
+        private void ReloadWaveform() {
+            if (pictureBox1.Width > 0 && pictureBox1.Height > 0) {
+                if (pictureBox1.Image != null)
+                    pictureBox1.Image.Dispose();
+                pictureBox1.Image = wg.CreateWaveform(pictureBox1.Width, pictureBox1.Height);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
